919e2402b8337323dac2016d149953d306be198f
shusakuarkt <64597572+shusakuarkt@users.noreply.github.com>
2020-11-27 06:11:10 +0900

    Update buhin.C

--- buhin.C
+++ buhin.C
@@ -10,7 +10,6 @@ void buhin(){
   double v1,v2,i1,i2;
   double t=0;
   int y=0;
-  //  base1->SetCs(1.*1e-10);
   base1->SetV0(4900);
   base1->SetR(1e+7);
   base1->Setr0(30);
@@ -89,103 +88,18 @@ void buhin(){
   voltage2->GetYaxis()->SetTitle("Voltage [V]");
   voltage2->GetYaxis()->SetTitleOffset(1.3);
   voltage2->SetTitle("voltage");
-  // voltage1->SetMarkerStyle(20);
-  // voltage1->SetMarkerColor(kBlue);
-  // voltage1->SetMarkerSize(0.5);
-  
+
   voltage2->Draw("AP");
-  //  voltage1->Draw("P");
+
 
   //凡例の表示
   TLegend* tl=new TLegend(0.5,0.2,0.89,0.3);
-  // tl->AddEntry(voltage1,"capaciter","p");
   tl->AddEntry(voltage2,"chamber","p");
   tl->SetTextSize(0.03);//プロットのための整形処理
   tl->Draw();
 
 
   gPad->Print("voltagetestkouhani.png");
-
-  //////////////////////////////////////////////////////
-
-  // int k=1;
-  // double v0v=base1->GetV0();
-  // for(int j=0;j<5;j++){
-  //   base1->SetR((2e+7)-(k)*1e+7);
-  //   v0v=base1->GetR();
-  //   t=0;
-  //   string v0=to_string(v0v);
-  //   for(int i=0; i<1000; i++){
-  //   t+=1e-11;
-  //   i1=base1->I1(t);
-  //   i2=base1->I2(t);
-  //   v1=base1->V1(i1);
-  //   v2=base1->V2(i1,i2);
-  //   voltage1->SetPoint(i,t*1e+9,v1);
-  //   current1->SetPoint(i,t*1e+9,i1);
-  //   voltage2->SetPoint(i,t*1e+9,v2);
-  //   current2->SetPoint(i,t*1e+9,i2);
-  // };
-
-
-  // /////////////////////////////////////////////////////
-
-  //   string plotname="voltage"+v0+"ohm";
-  // //プロットのための整形処理
-  // voltage2->SetMarkerStyle(20);
-  // voltage2->SetMarkerColor(kRed);
-  // voltage2->SetMarkerSize(0.5);
-  // voltage2->GetXaxis()->SetTitle("time[ns]");
-  // voltage2->GetYaxis()->SetTitle("Voltage [V]");
-  // voltage2->GetYaxis()->SetTitleOffset(1.3);
-  // voltage2->SetTitle(plotname.c_str());
-  // voltage1->SetMarkerStyle(20);
-  // voltage1->SetMarkerColor(kBlue);
-  // voltage1->SetMarkerSize(0.5);
-  
-  // voltage2->Draw("AP");
-  // voltage1->Draw("P");
-
-  // //凡例の表示
-  // TLegend* tl=new TLegend(0.5,0.2,0.89,0.3);
-  // tl->AddEntry(voltage1,"capaciter","p");
-  // tl->AddEntry(voltage2,"chamber","p");
-  // tl->SetTextSize(0.03);//プロットのための整形処理
-  // tl->Draw();
-  // string printname="voltage"+v0+"ohm.png";
-
-  // gPad->Print(printname.c_str());
-  // k++;
-  // }
-  /////////////////////////////////////////////////////
-
-  // //プロットのための整形処理
-  // current1->SetMarkerStyle(20);
-  // current1->SetMarkerColor(kRed);
-  // current1->SetMarkerSize(0.5);
-  // current1->GetXaxis()->SetTitle("time[ns]");
-  // current1->GetYaxis()->SetTitle("Current [A]");
-  // current1->GetYaxis()->SetTitleOffset(1.3);
-  // current1->SetTitle("current");
-  // current2->SetMarkerStyle(20);
-  // current2->SetMarkerColor(kBlue);
-  // current2->SetMarkerSize(0.5);
-  
-  // current1->Draw("P");
-  // current2->Draw("P");
-
-  // //凡例の表示
-  // TLegend* tl2=new TLegend(0.5,0.2,0.89,0.3);
-  // tl2->AddEntry(current1,"capaciter","p");
-  // tl2->AddEntry(current2,"chamber","p");
-  // tl2->SetTextSize(0.03);//プロットのための整形処理
-  // tl2->Draw();
-
-  // // current1->Draw("AP");
-  // // current2->Draw("same");
-  // gPad->Print("current_comparison.png");
-
-  // //  gPad->SetLogz();
 
 
 
